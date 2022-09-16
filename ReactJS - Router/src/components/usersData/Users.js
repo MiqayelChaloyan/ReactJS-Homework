e33c3cab9b4ffdf410a3f28d8a3c7b1616c2a145
Miqayel <miqochaloyan@gmail.com>
2022-09-16 20:50:09 +0400

    Added ReactJS - Router

---
+++ ReactJS - Router/src/components/usersData/Users.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import './users.css'
+
+/* Router */
+import { Link } from "react-router-dom";
+
+
+function Users() {
+    const [users, setUsers] = useState([]);
+
+    const BASE_URL = "http://localhost:8080";
+
+
+    useEffect(() => {
+        fetch(`${BASE_URL}/users?page=${1}&limit=${19}`)
+            .then(result => {
+                return result.json()
+            })
+            .then(data => {
+                setUsers(data)
+                console.log(data);
+            })
+    }, [])
+
+    return (
+        <div className='users-list'>
+
+            {users.map(user =>
+                <Link key={user.id} to={`/user/${user.id}`}>
+                    <li className='user'>Name - {user.name}</li>
+                </Link>
+            )}
+
+        </div>
+    )
+}
+
+export default Users;
